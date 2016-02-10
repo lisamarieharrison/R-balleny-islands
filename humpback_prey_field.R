9a37d807b0c44e38d01a7387a6e8d888617e15ee
Lisa-Marie Harrison uni <lisamarie.k.harrison@gmail.com>
2016-02-10 14:28:23 +1100

    Reading data from csv access files and subsetting to only HB

--- humpback_prey_field.R
+++ humpback_prey_field.R
@@ -3,3 +3,11 @@
 #date: 10/02/2016
 
 setwd(dir = "C:/Users/43439535/Documents/Lisa/phd/Balleny Islands/csv")
+
+gps      <- read.csv("GpsData.csv", header = T)
+sighting <- read.csv("Sighting.csv", header = T)
+env      <- read.csv("Environment.csv", header = T)
+effort   <- read.csv("Effort.csv", header = T)
+
+#subset sightings to only HB whales (HB = Species 07)
+sighting <- subset(sighting, Species == 07)
