1c7c3b76419862882c8c7b4cc09af83fda57c760
Lisa-Marie Harrison uni <lisamarie.k.harrison@gmail.com>
2016-02-10 15:43:12 +1100

    Subsetting effort by half, full and off effort

--- humpback_prey_field.R
+++ humpback_prey_field.R
@@ -11,3 +11,16 @@ effort   <- read.csv("Effort.csv", header = T)
 
 #subset sightings to only HB whales (HB = Species 07)
 sighting <- subset(sighting, Species == 07)
+
+#plot of sightings by date
+plot(table(sighting$Time), ylab = "Number of sightings", main = "HB sightings by date")
+
+#effort where MI observers present
+#next zero cell included to give total time on effort
+#doesn't include changes in observers
+effort <- effort[effort$NObserversM != c(tail(effort$NObserversM, -1), 2), ]
+effort <- effort[-1, ]
+
+gps_full <- effort$GpsIndex[effort$NObserversM == 2] #start of full effort
+gps_half <- effort$GpsIndex[effort$NObserversM == 1] #start of half effort
+gps_end  <- effort$GpsIndex[effort$NObserversM == 0] #end of effort
