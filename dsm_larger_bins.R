d49a569f453dc0f2bb6d2c15accfe23a6996c539
Lisa-Marie Harrison uni <lisamarie.k.harrison@gmail.com>
2016-05-16 16:33:18 +1000

    Script to further interpolate environmental data for larger bins

---
+++ dsm_larger_bins.R
@@ -0,0 +1,46 @@
+segdata_new <- NULL
+
+for (i in seq(1, nrow(segdata) - 1, by = 3)) {
+  
+  if (segdata$Transect.Label[i] == segdata$Transect.Label[i + 1] & segdata$Transect.Label[i] == segdata$Transect.Label[i + 2]) {
+    
+    temp <- colMeans(segdata[c(i:(i + 2)), ])
+    temp[5] <- sum(segdata[c(i:(i + 2)), 5])
+    temp[9] <- sum(segdata[c(i:(i + 2)), 9])
+    temp[14] <- mean(segdata[c(i:(i + 2)), 14])
+    temp[7] <- mean(segdata[c(i:(i + 2)), 7])
+    
+    segdata_new <- rbind(segdata_new, temp)
+    
+  }
+  
+}
+
+segdata_new[, 7] <- 1:nrow(segdata_new)
+segdata_new[, 14] <- chron(segdata_new[, 14])
+names(segdata_new) <- colnames(segdata_new)
+segdata_new <- as.data.frame(segdata_new)
+
+obsdata_new <- obsdata
+obsdata_new$Sample.Label <- ceiling(match(obsdata_new$Sample.Label, segdata$Sample.Label)/3)
+segment.area_new <- segdata_new[, 5] * (truncation_width - 200)*2
+obsdata_new <- obsdata_new[obsdata_new$distance >= 200 & obsdata_new$distance <= truncation_width, ]
+
+obsdata_new <- na.omit(obsdata_new)
+
+whale.dsm <- dsm(count ~ s(x, y, bs="sw", xt=list(bnd=bnd)) + log(krill) + s(density, bs = "cs") + s(bottom_depth, bs = "cs"), family = "poisson", ddf.obj = det_function, select=T, 
+                 segment.data = segdata_new, observation.data = obsdata_new, method = "REML", segment.area = segment.area_new,
+                 knots = soap.knots)
+
+
+summary(whale.dsm)
+
+plot(whale.dsm, pages = 1)
+
+
+
+plot(segdata_new$density, segdata_new$number)
+
+
+
+
